64bf081dee76ed23601a6a182f23988bbbb45e1b
Lukas Klement <lukas.klement@koerber.com>
2024-09-13 16:15:33 +0200

    add: vue framework and tailwindcss

---
+++ adventszauber/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class=" font-extrabold text-4xl">{{ msg }}</h1>
+    <h3>
+      You’ve successfully cloned our school project for the Talahons.
+      <div class="flex flex-row gap-2">
+        <p>IT22-B</p>
+        +
+        <p>HA24</p>
+      </div>
+    </h3>
+  </div>
+</template>
