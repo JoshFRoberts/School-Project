f345103e4db9aabcfc9031e34836150f25a9d6b9
Joshua Roberts <joshua.roberts@securepoint.de>
2024-09-20 11:28:32 +0200

    feat: adds customers colors to tailwind config

--- adventszauber/tailwind.config.js
+++ adventszauber/tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'th-black': '#0A0A0D',
+        'th-white': '#FCFAF5',
+        'th-primary': '#83639D',
+        'th-secondary': '#4C2F26',
+        'th-highlight': '#FF9B00',
+
+      },
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
