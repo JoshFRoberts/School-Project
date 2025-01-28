dc5fbe4951317cb5eb7329b4548ff9e4a77b3858
Lukas Klement <lukas.klement@koerber.com>
2025-01-28 15:26:56 +0100

    add rules page

--- src/components/Ueberuns.vue
+++ src/components/Ueberuns.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto px-6 py-8">
+    <div class="container mx-auto px-6 py-8 ">
       <h1 class="text-4xl font-bold text-center mb-8">Über uns</h1>
       <div class="dark:bg-dark-background-LightGray bg-background-LightGray text-text-Gray dark:text-dark-text-Gray rounded-lg shadow-md p-6">
         <p class="mb-4">Wir sind ein innovatives Unternehmen, das die Welt des 3D-Drucks mit der Freude an Brettspielen und saisonaler Dekoration verbindet. Unsere Mission ist es, einzigartige und hochwertige Produkte zu schaffen, die Freude und Kreativität in Ihr Zuhause bringen.</p>
