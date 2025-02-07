e8772911dc0bed05b9a2b1ccee1111ee1badbd8b
GyverMarc <GyverMarc@users.noreply.github.com>
2025-02-07 13:16:39 +0100

    wip: change about us page view

--- src/components/Ueberuns2.vue
+++ src/components/Ueberuns2.vue
@@ -1,44 +1,49 @@
 <template>
-    <div class="container mx-auto px-6 py-8">
-      <h1 class="text-4xl font-bold text-center mb-8">Über „Baller los“</h1>
-      
-      <p class="text-lg text-center mb-6">
-        Moin, wir sind die HA24B und das ist unsere Schülerfirma „Baller los", was so viel wie mit voller Kraft loslegen bedeutet – und das machen wir.
-      </p>
-  
-      <div class="flex flex-wrap items-center bg-background-LightGray dark:bg-dark-background-LightGray rounded-lg shadow-md">
-        <div class="w-full md:w-1/2 p-6 text-text-Gray dark:text-dark-text-Gray">
-          <h2 class="text-2xl font-semibold mb-4">Unsere Mission</h2>
-          <p class="mb-4">
-            Wir sind 24 Schülerinnen und Schüler im Alter von 15 bis 18 Jahren. 
-          </p>
-          <p class="mb-4">
-            Diese Schülerfirma haben wir im Rahmen der einjährigen Berufsfachschule Wirtschaft an der BBS I Lüneburg gegründet. 
-          </p>
-          <p class="mb-4">
-            Wir stellen ein eigenes Spiel her und wollen mit diesem Spiel auf den Umweltschutz, besonders bei den Gewässern, aufmerksam machen.
-          </p>
-          <p class="mb-4">
-            Daher ist es selbstverständlich, dass wir unser Spiel so umweltfreundlich und hochwertig wie möglich produzieren. 
-          </p>
-          <p class="mb-4">
-            Wir nutzen einen 3D-Drucker mit recyclefähigem und sogar Recycling-Filament, um unsere Figuren selbst herstellen zu können. Auch die anderen Materialien wie Spielbrett und Verpackung sind recyclefähig oder bestehen aus recyclingmaterial. 
-          </p>
-          <p class="mb-4">
-            Zusätzlich untersuchen wir die Verschmutzung der Ilmenau (dieser Fluss fließt durch Lüneburg), indem wir Plastikmüll einsammeln und zählen. 
-          </p>
-          <p class="mb-4">
-            Für diese Website kooperieren wir mit dem Lüneburger IT-Unternehmen „webnetz“ und den Auszubildenden zum Fachinformatiker im 3. Lehrjahr aus der Klasse IT22B von unserer Schule. 
-          </p>
-        </div>
-  
-        <div class="w-full md:w-1/2 p-6">
-            <img
-            src="@/assets/baller_los.svg"
-            alt="Baller Los Logo"
-            class="rounded-lg shadow-lg max-w-full h-auto"
-          />
-        </div>
+  <div class="container mx-auto px-6 py-8 overflow-auto bg-background-cream dark:bg-dark-background-cream">
+    <h1 class="text-4xl font-bold text-center mb-8">Über „Baller los“</h1>
+
+    <p class="text-lg text-center mb-6">
+      Moin, wir sind die HA24B und das ist unsere Schülerfirma „Baller los", was so viel wie mit voller Kraft loslegen
+      bedeutet – und das machen wir.
+    </p>
+
+    <div
+      class="flex flex-wrap items-center bg-background-LightGray dark:bg-dark-background-LightGray rounded-lg shadow-md">
+      <div class="w-full md:w-1/2 p-6 text-text-Gray dark:text-dark-text-Gray">
+        <h2 class="text-2xl font-semibold mb-4">Unsere Mission</h2>
+        <p class="mb-4">
+          Wir sind 24 Schülerinnen und Schüler im Alter von 15 bis 18 Jahren.
+        </p>
+        <p class="mb-4">
+          Diese Schülerfirma haben wir im Rahmen der einjährigen Berufsfachschule Wirtschaft an der BBS I Lüneburg
+          gegründet.
+        </p>
+        <p class="mb-4">
+          Wir stellen ein eigenes Spiel her und wollen mit diesem Spiel auf den Umweltschutz, besonders bei den
+          Gewässern, aufmerksam machen.
+        </p>
+        <p class="mb-4">
+          Daher ist es selbstverständlich, dass wir unser Spiel so umweltfreundlich und hochwertig wie möglich
+          produzieren.
+        </p>
+        <p class="mb-4">
+          Wir nutzen einen 3D-Drucker mit recyclefähigem und sogar Recycling-Filament, um unsere Figuren selbst
+          herstellen zu können. Auch die anderen Materialien wie Spielbrett und Verpackung sind recyclefähig oder
+          bestehen aus recyclingmaterial.
+        </p>
+        <p class="mb-4">
+          Zusätzlich untersuchen wir die Verschmutzung der Ilmenau (dieser Fluss fließt durch Lüneburg), indem wir
+          Plastikmüll einsammeln und zählen.
+        </p>
+        <p class="mb-4">
+          Für diese Website kooperieren wir mit dem Lüneburger IT-Unternehmen „webnetz“ und den Auszubildenden zum
+          Fachinformatiker im 3. Lehrjahr aus der Klasse IT22B von unserer Schule.
+        </p>
+      </div>
+
+      <div class="w-full md:w-1/2 p-6">
+        <img src="@/assets/images/Klassenfoto.jpg" alt="Klassenfoto" class="rounded-lg shadow-lg max-w-full h-auto" />
       </div>
     </div>
-  </template>
+  </div>
+</template>
